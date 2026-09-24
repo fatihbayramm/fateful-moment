d6afcb7f14e2e6d8016d504eb35b129c52043b83
fatih bayram <fatihbayramybs@gmail.com>
2026-09-24 20:57:14 +0300

    icons, images and colors setted.

---
+++ fateful-moment/constants/theme.ts
@@ -0,0 +1,6 @@
+const brandPrimary = "#00d3f3";
+const brandSecondary = "#0f172a";
+const brandRed = "fb2c36";
+const brandBackground = "#020617";
+const brandBorder = "1d293d";
+const brandText = "#f1f5f9";
